62cbcedc7f42af5a59109e1571a506a655b2e57e
Luca Borghini <lukino2000@gmail.com>
2017-07-06 17:11:35 +0200

    options to disable output

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 var es = require('event-stream');
 var spawn = require('child_process').spawn;
 var path = require('path');
+var gutil = require('gulp-util');
 
-module.exports = function(opts) {
+module.exports = function (opts) {
   opts = opts || {};
   opts.args = opts.args || [];
   opts.env = Object.assign({}, process.env, opts.env || {});
 
-  return es.map(function(script, next) {
+  var logError = function (msg) { gutil.log(gutil.colors.red(msg)) }
+  var logWarning = function (msg) { gutil.log(gutil.colors.yellow(msg)) }
+  var logInfo = function (msg) { gutil.log(gutil.colors.white(msg)) }
+  var log = function (msg) { gutil.log(msg) }
+  var empty = function(){};
+
+  if (!('stdout' in opts) || !opts.stdout) log = empty;
+  if (!('stderr' in opts) || !opts.stderr) logError = empty;
+
+  return es.map(function (script, next) {
     var compil = path.resolve(path.join(__dirname, 'inno/ISCC.exe'));
     var script_path = path.resolve(script.path);
 
     var args, run;
     if (process.platform !== 'win32') {
       args = [compil, 'Z:' + script_path].concat(opts.args);
-      run = spawn('wine', args, {env: opts.env});
+      run = spawn('wine', args, { env: opts.env });
     } else {
       args = [script_path].concat(opts.args);
-      run = spawn(compil, args, {env: opts.env});
+      run = spawn(compil, args, { env: opts.env });
     }
-    run.stdout.on('data', function(data) {
-      console.log('stdout: ' + data);
+    run.stdout.on('data', function (data) {
+      log('stdout: ' + data.toString().trim());
     });
-    run.stderr.on('data', function(data) {
-      console.log('stderr: ' + data);
+    run.stderr.on('data', function (data) {
+      logError('stderr: ' + data.toString().trim());
     });
-    run.on('close', function(code) {
-      var message = 'child process exited with code ' + code;
-      console.log(message);
+    run.on('close', function (code) {
       if (code !== 0) {
+        var message = 'child process exited with code ' + code;
+        logError(message);
         return next(message);
       } else {
         return next(null);
